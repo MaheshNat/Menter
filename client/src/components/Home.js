6c3922c0ef2dbaf4ffe353bcdf04e5d34bfac619
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-08-30 12:06:54 -0500

    Added sample emails in Home.js and fixed path error in app.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,13 +4,25 @@ export default class Home extends Component {
   render() {
     return (
       <>
-        <div className='jumbotron text-center' style={{ marginTop: '2em' }}>
+        <div className="jumbotron text-center" style={{ marginTop: '2em' }}>
           <h1>Menter</h1>
           <h3>
             Connect with students to help you with what you need, and help them
             with what you know.
           </h3>
           <h5>Made for students, by students.</h5>
+          <h2>
+            Sample Accounts If You Want To Test Out Invitations / Matching
+            Logic:
+          </h2>
+          <h3>Password for all accounts: 'bruh'</h3>
+          <ol>
+            <li>vilma.triana@gmail.com</li>
+            <li>kortney.rittenberry@gmail.com</li>
+            <li>karri.mullen@gmail.com</li>
+            <li>flavia.gutter@gmail.com</li>
+            <li>susan.scally@gmail.com</li>
+          </ol>
         </div>
       </>
     );
