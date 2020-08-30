d91521e9031b1d39ccd900ad7cb369170bf15311
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-08-30 03:59:45 -0500

    added SSL redirect

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ const app = express();
 const authRouter = require('./routes/auth');
 const invitationRouter = require('./routes/invitation');
 
+const sslRedirect = (environments = ['production'], status = 302) => {
+  const currentEnv = process.env.NODE_ENV;
+  const isCurrentEnv = environments.includes(currentEnv);
+  return (req, res, next) => {
+    if (isCurrentEnv) {
+      req.headers['x-forwarded-proto'] !== 'https'
+        ? res.redirect(status, 'https://' + req.hostname + req.originalUrl)
+        : next();
+    } else next();
+  };
+};
+
 // Register middleware
+app.use(sslRedirect());
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRouter);
