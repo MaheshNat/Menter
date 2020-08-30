e406090a99cd13e8bbab10a3f95781c4ebe961a6
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-08-30 12:39:06 -0500

    formatted Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,23 +6,22 @@ export default class Home extends Component {
       <>
         <div className="jumbotron text-center" style={{ marginTop: '2em' }}>
           <h1>Menter</h1>
-          <h3>
-            Connect with students to help you with what you need, and help them
-            with what you know.
-          </h3>
-          <h5>Made for students, by students.</h5>
+          <h3>Teach to learn.</h3>
+          <h5 style={{ marginBottom: '2em' }}>
+            Made for students, by students.
+          </h5>
           <h2>
             Sample Accounts If You Want To Test Out Invitations / Matching
             Logic:
           </h2>
           <h3>Password for all accounts: 'bruh'</h3>
-          <ol>
+          <ul>
             <li>vilma.triana@gmail.com</li>
             <li>kortney.rittenberry@gmail.com</li>
             <li>karri.mullen@gmail.com</li>
             <li>flavia.gutter@gmail.com</li>
             <li>susan.scally@gmail.com</li>
-          </ol>
+          </ul>
         </div>
       </>
     );
